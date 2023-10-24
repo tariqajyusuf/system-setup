5b1a73ab7b3e930a2df5b17a0fa46fca594ee604
Tariq Yusuf <tariq@tariqyusuf.in>
2023-10-24 16:46:44 -0700

    Add autocurl if we got this file directly

--- init.sh
+++ init.sh
@@ -6,9 +6,18 @@ exec > >(trap "" INT TERM; sed "s/^/[$SCRIPT_NAME] /")
 exec 2> >(trap "" INT TERM; sed "s/^/[$SCRIPT_NAME - error] /" >&2)
 
 echo "tariqajyusuf/system-setup ALPHA build"
+echo
+echo
 
-echo
-echo
+echo "Checking if repo was downloaded locally"
+if ! [ -d ".git" ]; then
+  echo "Repo not downloaded, fetching latest"
+  mkdir -p /tmp/system-setup
+  curl -L -O https://github.com/tariqajyusuf/system-setup/archive/refs/heads/main.zip
+  unzip main.zip -d /tmp/system-setup
+  cd /tmp/system-setup
+fi
+
 export UNAME=`uname | tr '[:upper:]' '[:lower:]'`
 echo "Setting up for $UNAME..."
 
