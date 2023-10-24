784d9ef22c7aa18f6f46f0cecdfedcc86df6f6c6
Tariq Yusuf <tariq@tariqyusuf.in>
2023-10-24 16:11:16 -0700

    Add homebrew installation

--- modules/oh-my-zsh/darwin.sh
+++ modules/oh-my-zsh/darwin.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+# Installs oh-my-zsh
+#
+# Expects setup/homebrew
 
+brew install zsh git
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
