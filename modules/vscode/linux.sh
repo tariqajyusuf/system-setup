f3d85b21cc920268ec306d97e713139919864cd2
Tariq Yusuf <tariq@tariqyusuf.in>
2023-10-24 18:49:39 -0700

    [vscode] fix output piping

--- modules/vscode/linux.sh
+++ modules/vscode/linux.sh
@@ -7,12 +7,12 @@ mkdir -p /tmp/system-setup/scratch/vscode
 cd /tmp/system-setup/scratch/vscode
 if which apt; then
   echo "[VSCode] Downloading VSCode for linux-deb-x64"
-  curl -L -O https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64 > vscode.deb &> $OUTPUT
+  curl -L -O https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64 > vscode.deb 2> $OUTPUT
   echo "[VSCode] Installing VSCode on your machine, you may get a sudo prompt"
   apt install vscode.deb &> $OUTPUT
 else
   echo "[VSCode] Downloading VSCode for linux64cli"
-  curl -L -O https://code.visualstudio.com/sha/download?build=stable&os=cli-alpine-x64 > vscode.tar.gz &> $OUTPUT
+  curl -L -O https://code.visualstudio.com/sha/download?build=stable&os=cli-alpine-x64 > vscode.tar.gz 2> $OUTPUT
   echo "[VSCode] Extracting files..."
   tar -xzf vscode.tar.gz &> $OUTPUT
   echo "[VSCode] Installing VSCode on your machine, you may get a sudo prompt"
