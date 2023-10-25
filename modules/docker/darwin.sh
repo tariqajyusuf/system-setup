838cc4aeea0cd92cf00ba3fb3cf9488ef2ce8c17
Tariq Yusuf <tariq@tariqyusuf.in>
2023-10-25 13:53:39 -0700

    Fix prompts

--- modules/docker/darwin.sh
+++ modules/docker/darwin.sh
@@ -8,8 +8,8 @@ rm -rf /tmp/system-setup/scratch/docker
 mkdir -p /tmp/system-setup/scratch/docker
 cd /tmp/system-setup/scratch/docker
 curl -L -O "https://desktop.docker.com/mac/main/$PLATFORM/Docker.dmg" >> docker.dmg &> $OUTPUT
-echo "[VSCode] Mounting image..."
+echo "[Docker] Mounting image..."
 hdiutil attach docker.dmg &> $OUTPUT
 cd /Volumes/Docker
-echo "[VSCode] Installing Docker on your machine, you may get a sudo prompt"
+echo "[Docker] Installing Docker on your machine, you may get a sudo prompt"
 sudo mv Docker.app/ /Applications &> $OUTPUT
