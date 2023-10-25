8250a54ad95eb89c6f0a3af8801670b53d90651e
Tariq Yusuf <tariq@tariqyusuf.in>
2023-10-24 18:29:07 -0700

    Add VSCode

---
+++ modules/vscode/darwin.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Visual Studio Code (https://code.visualstudio.com)
+
+echo "[VSCode] Downloading VSCode for darwin-universal"
+curl -L -O https://code.visualstudio.com/sha/download\?build\=stable\&os\=darwin-universal >> vscode.zip
+echo "[VSCode] Extracting files..."
+unzip vscode.zip
+echo "[VSCode] Installing VSCode on your machine, you may get a sudo prompt"
+sudo mv Visual\ Studio\ Code.app/ /Applications
---
+++ modules/vscode/linux.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# Visual Studio Code (https://code.visualstudio.com)
+
+if [ which apt ] ; then
+  echo "[VSCode] Downloading VSCode for linux-deb-x64"
+  curl -L -O https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64 >> vscode.deb
+  echo "[VSCode] Installing VSCode on your machine, you may get a sudo prompt"
+  sudo apt install vscode.deb
+else
+  echo "[VSCode] Downloading VSCode for linux64cli"
+  curl -L -O https://code.visualstudio.com/sha/download?build=stable&os=cli-alpine-x64 >> vscode.tar.gz
+  echo "[VSCode] Extracting files..."
+  tar
+  echo "[VSCode] Installing VSCode on your machine, you may get a sudo prompt"
+  mv code /usr/bin
+fi
