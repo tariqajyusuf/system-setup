d9eee81c00223e8f590d9c3bad66736c75c7810f
Tariq Yusuf <tariq@tariqyusuf.in>
2023-10-24 18:45:18 -0700

    temporarily remove sudo

--- modules/vscode/darwin.sh
+++ modules/vscode/darwin.sh
@@ -6,5 +6,5 @@ curl -L -O https://code.visualstudio.com/sha/download\?build\=stable\&os\=darwin
 echo "[VSCode] Extracting files..."
 unzip vscode.zip &> $OUTPUT
 echo "[VSCode] Installing VSCode on your machine, you may get a sudo prompt"
-sudo mv Visual\ Studio\ Code.app/ /Applications &> $OUTPUT
+mv Visual\ Studio\ Code.app/ /Applications &> $OUTPUT
 echo "[VSCode] Installed! Log into Settings Sync to pull your settings."
--- modules/vscode/linux.sh
+++ modules/vscode/linux.sh
@@ -4,12 +4,12 @@
 echo "[VSCode] Detecting linux version"
 if which apt; then
   echo "[VSCode] Downloading VSCode for linux-deb-x64"
-  curl -L -O https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64 >> vscode.deb &> $OUTPUT
+  curl -L -O https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64 > vscode.deb &> $OUTPUT
   echo "[VSCode] Installing VSCode on your machine, you may get a sudo prompt"
-  sudo apt install vscode.deb &> $OUTPUT
+  apt install vscode.deb &> $OUTPUT
 else
   echo "[VSCode] Downloading VSCode for linux64cli"
-  curl -L -O https://code.visualstudio.com/sha/download?build=stable&os=cli-alpine-x64 >> vscode.tar.gz &> $OUTPUT
+  curl -L -O https://code.visualstudio.com/sha/download?build=stable&os=cli-alpine-x64 > vscode.tar.gz &> $OUTPUT
   echo "[VSCode] Extracting files..."
   tar -xzf vscode.tar.gz &> $OUTPUT
   echo "[VSCode] Installing VSCode on your machine, you may get a sudo prompt"
