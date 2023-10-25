c0fe05969cdd5fa6b1ecf805f8e654a563e8bc51
Tariq Yusuf <tariq@tariqyusuf.in>
2023-10-24 21:09:51 -0700

    Detect platform for installation

--- modules/vscode/linux.sh
+++ modules/vscode/linux.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 # Visual Studio Code (https://code.visualstudio.com)
 
+PLATFORM=`uname -m`
+
+# Special case for aarch64 which should be treated as just ARM
+if [ $PLATFORM -eq "aarch64" ]; then PLATFORM="arm64"; fi
+
 echo "[VSCode] Detecting linux version"
 rm -rf /tmp/system-setup/scratch/vscode
 mkdir -p /tmp/system-setup/scratch/vscode
 cd /tmp/system-setup/scratch/vscode
 if which apt; then
-  echo "[VSCode] Downloading VSCode for linux-deb-x64"
-  curl -L -o vscode.deb "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64" &> $OUTPUT
+  echo "[VSCode] Downloading VSCode for linux-deb-$PLATFORM"
+  curl -L -o vscode.deb "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-$PLATFORM" &> $OUTPUT
   echo "[VSCode] Installing VSCode on your machine, you may get a sudo prompt"
   apt install ./vscode.deb &> $OUTPUT
 else
   echo "[VSCode] Downloading VSCode for linux64cli"
-  curl -L -o vscode.deb "https://code.visualstudio.com/sha/download?build=stable&os=cli-alpine-x64" &> $OUTPUT
+  curl -L -o vscode.deb "https://code.visualstudio.com/sha/download?build=stable&os=cli-alpine-$PLATFORM" &> $OUTPUT
   echo "[VSCode] Extracting files..."
   tar -xzf vscode.tar.gz &> $OUTPUT
   echo "[VSCode] Installing VSCode on your machine, you may get a sudo prompt"
