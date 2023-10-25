18ea5558560da97a72f6e3b11ffe5c3bb89236f4
Tariq Yusuf <tariq@tariqyusuf.in>
2023-10-24 21:18:06 -0700

    [iterm] add iterm for darwin

---
+++ modules/iterm/darwin.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Visual Studio Code (https://code.visualstudio.com)
+
+echo "[iTerm] Downloading iTerm..."
+rm -rf /tmp/system-setup/scratch/iterm
+mkdir -p /tmp/system-setup/scratch/iterm
+cd /tmp/system-setup/scratch/iterm
+curl -L -o iterm.zip "https://iterm2.com/downloads/stable/latest" &> $OUTPUT
+echo "[iTerm] Extracting files..."
+unzip iterm.zip &> $OUTPUT
+echo "[iTerm] Installing iTerm on your machine, you may get a sudo prompt"
+mv iTerm.app/ /Applications &> $OUTPUT
