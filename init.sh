129ed68e92e6b36c3fd2d0368461aac2f5fe78da
Tariq Yusuf <tariq@tariqyusuf.in>
2023-10-24 18:04:22 -0700

    Set verbose mode

--- init.sh
+++ init.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+OUTPUT=/dev/null
+VERBOSE=`false`
+
+while getopts v flag
+do
+  case "${flag}" in
+    v) VERBOSE=`true`;;
+  esac
+done
+
+if $VERBOSE: then; OUTPUT=/dev/fd/1; fi
 
 echo "tariqajyusuf/system-setup ALPHA build"
 echo
@@ -9,16 +20,16 @@ if ! [ -d ".git" ]; then
   echo "Repo not downloaded, fetching latest"
   rm -rf /tmp/system-setup
   mkdir -p /tmp/system-setup
-  curl -L -O https://github.com/tariqajyusuf/system-setup/archive/refs/heads/main.zip
+  curl -L -O https://github.com/tariqajyusuf/system-setup/archive/refs/heads/main.zip &> $OUTPUT
   if ! which unzip; then
     if which apt; then
-      apt install unzip
+      apt install unzip &> $OUTPUT
     else
       echo "unzip was not found, please install unzip"
       exit 1
     fi
   fi
-  unzip main.zip -d /tmp/system-setup
+  unzip main.zip -d /tmp/system-setup &> $OUTPUT
   cd /tmp/system-setup/system-setup-main
 fi
 
@@ -27,15 +38,18 @@ echo "Setting up for $UNAME..."
 
 echo "Running setup scripts..."
 for setup in `ls setup/*/$UNAME.sh`; do
-  sh -c $setup
+  echo "Installing $setup..."
+  sh -c $setup &> $OUTPUT
 done
 
 echo "Running module scripts..."
 for module in `ls modules/*/$UNAME.sh`; do
-  sh -c $module
+  echo "Installing $module..."
+  sh -c $module &> $OUTPUT
 done
 
 echo "Applying config preferences..."
 for patch in `ls config/*/*.patch`; do
-  sh -c "cd ~ && patch -i `pwd`/$patch"
+  echo "Patching $patch..."
+  sh -c "cd ~ && patch -i `pwd`/$patch" &> $OUTPUT
 done
