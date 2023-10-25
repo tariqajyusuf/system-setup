24fbb6b953f3328a90dc7b350a44b0ddefb678a5
Tariq Yusuf <tariq@tariqyusuf.in>
2023-10-25 11:28:59 -0700

    Add displaylink for darwin

---
+++ modules/displaylink/darwin.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# DisplayLink Drivers for Mac (https://www.synaptics.com/products/displaylink-graphics/)
+
+echo "[DisplayLink] Downloading installer..."
+rm -rf /tmp/system-setup/scratch/displaylink
+mkdir -p /tmp/system-setup/scratch/displaylink
+cd /tmp/system-setup/scratch/displaylink
+curl -L -O installer.pkg "https://www.synaptics.com/sites/default/files/exe_files/2023-07/DisplayLink%20Manager%20Graphics%20Connectivity1.9-EXE.pkg" &> $OUTPUT
+echo "[VSCode] Installing VSCode on your machine, you may get a sudo prompt"
+installer -pkg installer.pkg -target / &> $OUTPUT
