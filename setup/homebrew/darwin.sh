bb7b5e094b33658e846d0c5fc554d17a466c431c
Tariq Yusuf <tariq@tariqyusuf.in>
2024-04-13 07:53:59 -0700

    Add missing shell quote

--- setup/homebrew/darwin.sh
+++ setup/homebrew/darwin.sh
@@ -3,4 +3,4 @@
 echo "[darwin] Installing homebrew..."
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" &> $OUTPUT
 (echo; echo 'eval "$(/ops/homebrew/bin/brew shellenv)"') >> ~/.zprofile
-eval "$(/opt/homebrew/bin/brew shellenv)
+eval "$(/opt/homebrew/bin/brew shellenv)"
