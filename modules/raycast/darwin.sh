cd2a807ef711bc66edd97112bc1a98d62e54a9cc
Tariq Yusuf <c-tariqy@remitly.com>
2024-08-14 10:17:05 -0700

    Add Raycast

---
+++ modules/raycast/darwin.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Raycast (https://raycast.com/)
+
+echo "[Raycast] Downloading Raycast..."
+rm -rf /tmp/system-setup/scratch/raycast
+mkdir -p /tmp/system-setup/scratch/raycast
+cd /tmp/system-setup/scratch/raycast
+echo "[Raycast] Installing via homebrew..."
+brew install --cask raycast &> $OUTPUT
