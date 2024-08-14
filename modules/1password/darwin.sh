100cb3c9ea9fecc4fa8ba69d10fbd249a7921717
Tariq Yusuf <c-tariqy@remitly.com>
2024-08-14 10:29:09 -0700

    Silence outputs for 1P

--- modules/1password/darwin.sh
+++ modules/1password/darwin.sh
@@ -6,9 +6,9 @@ rm -rf /tmp/system-setup/scratch/1password
 mkdir -p /tmp/system-setup/scratch/1password
 cd /tmp/system-setup/scratch/1password
 curl -L -o iterm.zip "https://downloads.1password.com/mac/1Password.zip" &> $OUTPUT
-echo "[1Password] Extracting files..."
+echo "[1Password] Extracting files..." &> $OUTPUT
 unzip iterm.zip &> $OUTPUT
 echo "[1Password] Installing 1Password on your machine, you may get a sudo prompt"
 open -n ./*.app &> $OUTPUT
 echo "[1Password] Installing CLI..."
-brew install 1password-cli
+brew install 1password-cli &> $OUTPUT
