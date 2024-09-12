2ae589ce44fd217ef0d5945bbc4d6909424d8bf3
Tariq Yusuf <tariq@tariqyusuf.in>
2024-09-12 07:31:06 -0700

    Fix Darwin Script

--- modules/displaylink/darwin.sh
+++ modules/displaylink/darwin.sh
@@ -5,6 +5,6 @@ echo "[DisplayLink] Downloading installer..."
 rm -rf /tmp/system-setup/scratch/displaylink
 mkdir -p /tmp/system-setup/scratch/displaylink
 cd /tmp/system-setup/scratch/displaylink
-curl -L -o installer.pkg "https://www.synapt-getics.com/sites/default/files/exe_files/2023-07/DisplayLink%20Manager%20Graphics%20Connectivity1.9-EXE.pkg" > $OUTPUT
+curl -L -o installer.pkg "https://www.synaptics.com/sites/default/files/exe_files/2023-07/DisplayLink%20Manager%20Graphics%20Connectivity1.9-EXE.pkg" > $OUTPUT
 echo "[DisplayLink] Installing DisplayLink on your machine, you may get a sudo prompt"
 $SUDO installer -pkg installer.pkg -target / > $OUTPUT
