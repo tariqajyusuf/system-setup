a605b5f8650a3f10b64b3b5005a2d70fd4f9e327
Tariq Yusuf <tariq@tariqyusuf.in>
2024-12-04 18:09:07 -0800

    Add bash profile init

--- setup/homebrew/darwin.sh
+++ setup/homebrew/darwin.sh
@@ -4,5 +4,7 @@ echo "[darwin] Installing homebrew..."
 $SUDO xcodebuild -license accept
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" > $OUTPUT
 echo >> /Users/tariq/.zprofile
-    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/tariq/.zprofile
-    eval "$(/opt/homebrew/bin/brew shellenv)"
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/tariq/.zprofile
+echo >> /Users/tariq/.bash_profile
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/tariq/.bash_profile
+eval "$(/opt/homebrew/bin/brew shellenv)"
