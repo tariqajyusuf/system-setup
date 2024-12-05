ca9afb43e6383a1ceda55e436126bd655515d784
Tariq Yusuf <tariq@tariqyusuf.in>
2024-12-04 18:05:26 -0800

    Add home-brew init to script

--- setup/homebrew/darwin.sh
+++ setup/homebrew/darwin.sh
@@ -3,5 +3,6 @@
 echo "[darwin] Installing homebrew..."
 $SUDO xcodebuild -license accept
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" > $OUTPUT
-(echo; echo 'eval "$(/ops/homebrew/bin/brew shellenv)"') >> ~/.zprofile
-eval "$(/opt/homebrew/bin/brew shellenv)"
+echo >> /Users/tariq/.zprofile
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/tariq/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
